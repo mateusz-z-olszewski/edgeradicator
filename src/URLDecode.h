ed7dc470ac16d5881fa4f215b642ce333395a3ab
Mateusz Olszewski <matix2k4@outlook.com>
2022-07-17 01:16:00 -0700

    [Version 1.0.2 (3)]
    Decoding URLs no longer depends on curl.

--- src/URLDecode.h
+++ src/URLDecode.h
@@ -1,14 +1,35 @@
 #pragma once
 
-#include <curl/curl.h>
-#include <string>
+#include <sstream>
 
-CURL* curl;
+std::string HEXCHARS = "1234567890ABCDEFabcdef";
 
-std::string URLDecode(std::string arg) {
-	return curl_easy_unescape(curl, arg.c_str(), 0, NULL);
+static char _Decode(char fst, char snd) {
+    unsigned int x;
+    std::stringstream ss;
+    std::string s{ fst, snd };
+    ss << std::hex << s;
+    ss >> x;
+    return (char)x;
 }
-
-void URLDecodeInit() {
-	curl = curl_easy_init();
+static bool _CheckInput(char x) {
+    return HEXCHARS.find(x) < 0;
+}
+std::string URLDecode(std::string arg) {
+    const size_t LEN = arg.length();
+    std::string out = "";
+    for (size_t i = 0; i < LEN; i++)
+    {
+        if (arg[i] != '%') {
+            out += arg[i];
+        }
+        else {
+            char fst, snd;
+            if (_CheckInput(fst = arg[i + 1])) return out;
+            if (_CheckInput(snd = arg[i + 2])) return out;
+            out += _Decode(fst, snd);
+            i += 2;
+        }
+    }
+    return out;
 }
